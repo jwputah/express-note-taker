94ab81c0549916856614077b9158a94bdc0fd963
jwputah <jwputah7@gmail.com>
2022-03-06 16:18:09 -0700

    add delete function to server.js

--- server.js
+++ server.js
@@ -5,28 +5,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./Develop/db/db');
-// const apiRoutes = require('./Develop/public/assets/js/index.js');
-// const htmlRoutes = require('./Develop/public/index.html');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// middleware function to serve static files in public folder
 app.use(express.static('Develop/public'));
 
 // Generate a unique id 
 const generateUniqueId = require('generate-unique-id');
-
-// function to create a note
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, "./Develop/db/db.json"),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}
 
 // Api routes
 app.get('/', (req, res) => {
@@ -46,6 +34,25 @@ app.post('/api/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
+
+// Delete note function
+app.delete('api/notes/id', (req, res) => {
+    const {id} = req.params;
+    const delNote = notes.findIndex(note => note.id == id);
+    notes.splice(delNote, 1);
+    return res.send();
+});
+
+// function to create a note
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, "./Develop/db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);

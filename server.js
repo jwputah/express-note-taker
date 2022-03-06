df80d88eea2af168ca596f42f954d581edc24bdd
jwputah <jwputah7@gmail.com>
2022-03-06 14:28:54 -0700

    file path server.js

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ function newNote(body, notesArray) {
 
 // Api routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
